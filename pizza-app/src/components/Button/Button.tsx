376523b33ae2220dab46bafb96c73a91db10c2a6
Yury Sidyakin <yurafikys@gmail.com>
2024-02-25 22:00:05 +0300

    feat: подключил шрифты и основные цвета проекта

---
+++ pizza-app/src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import cn from 'classnames';
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
+
+function Button({ children, className, ...props }: ButtonProps) {
+	return (
+		<button
+			className={cn(styles['button'], styles['accent'], className)}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
