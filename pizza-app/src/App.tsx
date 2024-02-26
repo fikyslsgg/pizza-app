080149a9fc7285e958306298d0735fe72b462253
Yury Sidyakin <yurafikys@gmail.com>
2024-02-26 13:09:51 +0300

    feat: Добавил компонент Button и Input

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,9 +1,14 @@
 import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
 	return (
 		<>
 			<Button onClick={() => console.log('123')}>Кнопка</Button>
+			<Button appearence='big' onClick={() => console.log('123')}>
+				Кнопка
+			</Button>
+			<Input placeholder='Email' />
 		</>
 	);
 }
