a3f02bd1efb12a0e46fdb0265e01e25e932075ac
Yury Sidyakin <yurafikys@gmail.com>
2024-03-06 13:34:52 +0300

    feat: Добавил компонент Search (input поиска)

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -1,9 +1,14 @@
 import Headling from '../../components/Headling/Headling';
+import Search from '../../components/Search/Search';
+import styles from './Menu.module.css';
 
 export function Menu() {
 	return (
 		<>
-			<Headling>Меню</Headling>
+			<div className={styles['head']}>
+				<Headling>Меню</Headling>
+				<Search placeholder='Введите блюдо или состав' />
+			</div>
 		</>
 	);
 }
