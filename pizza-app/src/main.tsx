f63685f900efb8e0988feadfc74a0a6508cd1294
Yury Sidyakin <yurafikys@gmail.com>
2024-03-26 11:53:17 +0300

    feat: Создал Layout для входа и регистрации

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { PREFIX } from './helpers/API.ts';
 import './index.css';
+import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
 import { Layout } from './layout/Menu/Layout.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
+import { Login } from './pages/Login/Login';
 import { Product } from './pages/Product/Product.tsx';
+import { Register } from './pages/Register/Register.tsx';
 
 const Menu = lazy(() => import('./pages/Menu/Menu.tsx'));
 
@@ -36,6 +39,20 @@ const router = createBrowserRouter([
 					const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
 					return data;
 				},
+			},
+		],
+	},
+	{
+		path: '/auth',
+		element: <AuthLayout />,
+		children: [
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: 'register',
+				element: <Register />,
 			},
 		],
 	},
