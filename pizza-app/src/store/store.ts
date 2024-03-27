7a64721fb2431447f0efa4f098bd6671d8c490fd
Yury Sidyakin <yurafikys@gmail.com>
2024-03-27 19:42:59 +0300

    feat: Добавил userSlice

--- pizza-app/src/store/store.ts
+++ pizza-app/src/store/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './user.slice';
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		user: userSlice,
+	},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export typeDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
